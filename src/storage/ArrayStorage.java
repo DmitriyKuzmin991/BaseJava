b1562c22e7dde3a35bc6406bb19fe960c281207a
DmitriyK <Dmitriykuzminn@gmail.com>
2024-03-30 23:47:53 +0300

    Implements abstract methods add, remove.

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -6,29 +6,17 @@ import model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-    public void save(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (storage.length == countResumes) {
-            System.out.println("Сохранить не удалось. Архив полон.");
-        } else if (isExisted(index)) {
-            System.out.println("Резюме с id: " + r.getUuid() + " уже есть в архиве");
-        } else {
-            storage[countResumes] = r;
-            countResumes++;
-            System.out.println("Резюме с id: " + r.getUuid() + " сохранено");
-        }
+    @Override
+    protected void add(int index, Resume resume) {
+        storage[countResumes] = resume;
+        countResumes++;
     }
 
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (isExisted(index)) {
-            storage[index] = storage[countResumes - 1];
-            storage[countResumes - 1] = null;
-            countResumes--;
-            System.out.println("Резюме с id: " + uuid + " удалено");
-        } else {
-            System.out.println("Резюме с id: " + uuid + " не найденно");
-        }
+    @Override
+    protected void remove(int index) {
+        storage[index] = storage[countResumes - 1];
+        storage[countResumes - 1] = null;
+        countResumes--;
     }
 
     protected int getIndex(String uuid) {
