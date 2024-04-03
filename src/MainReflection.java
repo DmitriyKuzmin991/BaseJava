52c9bde294558280c5ddef432afe5f7f0f414653
DmitriyK <Dmitriykuzminn@gmail.com>
2024-04-03 23:33:04 +0300

    Created class reflection and invoke method to string

---
+++ src/MainReflection.java
@@ -0,0 +1,20 @@
+import model.Resume;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Resume r = new Resume();
+        Method[] methods = r.getClass().getMethods();
+        for (Method method: methods) {
+            if(method.getName().equals("toString")){
+                System.out.println(method.invoke(r));
+            }
+        }
+        System.out.println("===============================================");
+        Method method = r.getClass().getMethod("toString");
+        System.out.println(method.invoke(r));
+
+    }
+}
