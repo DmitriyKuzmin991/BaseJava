c8d30f9c8e447c66ea66af2e92f9a52bdf787265
DmitriyK <Dmitriykuzminn@gmail.com>
2024-04-13 22:12:45 +0300

    create new ListStorage class, using ArrayList collection

---
+++ src/storage/ListStorage.java
@@ -0,0 +1,54 @@
+package storage;
+
+import model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+    private final List<Resume> storage = new ArrayList<>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    protected Resume getResume(int index) {
+        return storage.get(index);
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.toArray(new Resume[0]);
+    }
+
+    @Override
+    protected void insertResume(int index, Resume resume) {
+        storage.add(resume);
+    }
+
+    @Override
+    protected void removeResume(int index) {
+        storage.remove(index);
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        return storage.indexOf(new Resume(uuid));
+    }
+
+    @Override
+    protected void updateResume(int index, Resume resume) {
+        storage.set(index, resume);
+    }
+
+    @Override
+    protected void checkLimit(String uuid) {
+    }
+}
