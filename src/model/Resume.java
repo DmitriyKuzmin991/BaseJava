409e10d00bf8cf80c280143a15913d26eab15dc1
DmitriyK <Dmitriykuzminn@gmail.com>
2024-04-20 21:31:17 +0300

    Added new field fullName. Getter and setter for this field. Create two new constructor.

--- src/model/Resume.java
+++ src/model/Resume.java
@@ -8,11 +8,18 @@ import java.util.UUID;
  */
 public class Resume implements Comparable<Resume> {
     private final String uuid;
+    private String fullName;
     public Resume() {
-        this(UUID.randomUUID().toString());
+        this.uuid = (UUID.randomUUID().toString());
+        this.fullName = "JohnSimple";
+    }
+    public Resume(String uuid) {
+        this.uuid = uuid;
+        this.fullName = "JohnSimple";
     }
 
-    public Resume(String uuid) {
+    public Resume(String uuid, String fullName) {
+        this.fullName = fullName;
         this.uuid = uuid;
     }
 
@@ -22,7 +29,10 @@ public class Resume implements Comparable<Resume> {
 
     @Override
     public String toString() {
-        return uuid;
+        return "Resume{" +
+                "uuid='" + uuid + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
     }
 
     @Override
@@ -30,16 +40,24 @@ public class Resume implements Comparable<Resume> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Resume resume = (Resume) o;
-        return Objects.equals(uuid, resume.uuid);
+        return Objects.equals(uuid, resume.uuid) && Objects.equals(fullName, resume.fullName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(uuid);
+        return Objects.hash(uuid, fullName);
     }
 
     @Override
     public int compareTo(Resume r) {
         return this.getUuid().compareTo(r.getUuid());
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 }
